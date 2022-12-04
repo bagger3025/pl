f50cfa63ed6f252ba6a44f667fe8266da9790f4a
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-04 05:36:14 +0000

    04-1. Control it all from the CLI

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ import torchvision.models as models
 
 import pytorch_lightning as pl
 
+from pytorch_lightning.cli import LightningCLI
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 
@@ -74,78 +75,7 @@ class LitAutoEncoder(pl.LightningModule):
         return optimizer
 
 
-class CIFAR10Classifier(pl.LightningModule):
-    def __init__(self):
-        # init the pretrained LightningModule
-        self.feature_extractor = LitAutoEncoder.load_from_checkpoint(
-            "some/path")
-        self.feature_extractor.freeze()
-
-        # the autoencoder outputs a 100-dim representation and CIFAR-10 has 10 classes
-        self.classifier = nn.Linear(100, 10)
-
-    def forward(self, x):
-        representations = self.feature_extractor(x)
-        x = self.classifier(representations)
-        # ...
-
-
-class ImagenetTransferLearning(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-
-        # init a pretrained resnet
-        backbone = models.resnet50(weights="DEFAULT")
-        num_filters = backbone.fc.in_features
-        layers = list(backbone.children())[:-1]
-        self.feature_extractor = nn.Sequential(*layers)
-
-        # use the pretrained model to classify cifar-10 (10 image classes)
-        num_target_classes = 10
-        self.classifier = nn.Linear(num_filters, num_target_classes)
-
-    def forward(self, x):
-        self.feature_extractor.eval()
-        with torch.no_grad():
-            representations = self.feature_extractor(x).flatten(1)
-        x = self.classifier(representations)
-        # ...
-
-
-def finetune_imagenet_transferlearning():
-    def some_images_from_cifar10():
-        return
-
-    model = ImagenetTransferLearning()
-    trainer = pl.Trainer()
-    trainer.fit(model)
-
-    model = ImagenetTransferLearning.load_from_checkpoint("some/path")
-    model.freeze()
-
-    x = some_images_from_cifar10()
-    predictions = model(x)
-
-
-class BertMNLIFinetuner(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-
-        # from huggingface
-        self.bert = BertModel.from_pretrained(
-            "bert-base-cased", output_attentions=True)
-        self.W = nn.Linear(bert.config.hidden_size, 3)
-        self.num_classes = 3
-
-    def forward(self, input_ids, attention_mask, token_type_ids):
-
-        h, _, attn = self.bert(
-            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-
-        h_cls = h[:, 0]
-        logits = self.W(h_cls)
-        return logits, attn
-
+cli = LightningCLI(LitAutoEncoder)
 
 # Load data sets
 transform = transforms.ToTensor()
